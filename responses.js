6d635a1c32309f248ed8d2bbc6a4323869782334
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-11-01 19:46:00 +1300

    update

--- responses.js
+++ responses.js
@@ -1,17 +1,37 @@
 export const responses = {
-    howtoorganise: `So you think all the current comps suck?
+    howtoorganise: `
+__So you think all the current comps suck?__
 So be it. Time to organise one yourself. 
 (A brief guide for the eternally unimpressed.)
 
 **1. Find a venue**
-You want better comps? Start by finding a large open space. It should have around 4m² per competitor. Go beyond what you can find through Google. Places without a "Venue hire" page on their website are likely to be significantly cheaper than those that do.
+You want better comps? Start by finding a large open space. It should have around 4m² per competitor. 
+Go beyond what you can find through Google. Places *without* a "Venue hire" page on their website are likely to be significantly cheaper than those that do. 
 
 **2. Contact your local delegate**
-Tell them what you want to do (date, venue, events, etc), and pretend you've got a decent idea of the details. Oh so, you've only got complaints? Maybe don't email just yet.
+Tell them what you want to do (date, venue, events, etc), and pretend you've got a decent idea of the details. 
+Oh so, you've only got complaints? Maybe don't email just yet.
 
-**3. Find a WCA Delegate**
+**3.  Find a WCA Delegate**
 Without a Delegate, you can't have a competition. Delegates are volunteers, not your personal competition slave.
-You could try SEEK, but https://www.worldcubeassociation.org/delegates might be more effective.`,
+You could try SEEK, but https://www.worldcubeassociation.org/delegates will probably be more effective. 
+
+**4. Work with your delegate**
+They will help you build a schedule that makes sense and will work. 
+As you'll be shocked to hear, you can't only have 7x7 and 4x4 BLD. 
+Trust the process.
+
+**5. Run the comp**
+On the day of the comp, arrive early (generally, about an hour early) and help with venue setup. Do the things your Delegate asks you to. Don't be an inconvenience.
+Try not to be the problem you spend all your free time complaining about. If nothing else, being the problem you complain about is just plain ironic.
+
+**6. Clean up**
+Tables and chairs go back. Make the venue look how it looked when you walked in.
+Empty the rubbish bins. Don't ghost - be helpful.
+Congrats. You've officially done more than complain. 
+
+Maybe now you'll understand why competition feedback can't just be "just make the comps better". 
+`,
 
     // Aliases
     fixthesuckycomps: null,
